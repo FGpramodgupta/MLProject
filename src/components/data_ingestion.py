9eaf2eb5150cc67391199e2e003ee08e31d8bfe6
Pramod Gupta <pramod gupta>
2023-03-21 15:32:40 +0530

    Data Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,11 +1,17 @@
-import os 
+import os
 import sys
 from src.exception import CustomException
 from src.logger import logging
-
 import pandas as pd
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -47,4 +53,8 @@ if __name__ == "__main__":
     
     trans_obj = DataTransformation()
     
-    trans_obj.initiate_data_transformation(train_data,test_data)
+    train_arr, test_arr,_ = trans_obj.initiate_data_transformation(train_data,test_data)
+    
+    model_trainer = ModelTrainer()
+    
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
